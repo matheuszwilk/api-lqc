fdf6c3dba453ae5d2f9031454ab1c8a29e0afa82
matheuszwilk <matheuszwilk@gmail.com>
2024-03-26 12:11:19 -0300

    feat: ensure CreateAvaliationController and createAvaliationSchema

---
+++ src/controllers/avaliation/create-avaliation.js
@@ -0,0 +1,41 @@
+import { UserNotFoundError } from '../../errors/user.js'
+import { createAvaliationSchema } from '../../schemas/avaliation.js'
+import {
+    badRequest,
+    created,
+    serverError,
+    userNotFoundResponse,
+} from '../helpers/index.js'
+import { ZodError } from 'zod'
+
+export class CreateAvaliationController {
+    constructor(createAvaliationUseCase) {
+        this.createAvaliationUseCase = createAvaliationUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const params = httpRequest.body
+
+            await createAvaliationSchema.parseAsync(params)
+
+            const avaliation =
+                await this.createAvaliationUseCase.execute(params)
+
+            return created(avaliation)
+        } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                })
+            }
+
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse()
+            }
+
+            console.error(error)
+            return serverError()
+        }
+    }
+}
---
+++ src/schemas/avaliation.js
@@ -0,0 +1,63 @@
+import { z } from 'zod'
+import validator from 'validator'
+
+export const createAvaliationSchema = z.object({
+    user_lqc_id: z
+        .string({
+            required_error: 'User ID is required.',
+        })
+        .uuid({
+            message: 'User ID must be a valid UUID.',
+        }),
+    type: z.enum(
+        [
+            'CLEANING_5S',
+            'DISCIPLINE',
+            'COMMITMENT',
+            'IMPROVEMENTS',
+            'AUDITS',
+            'DETECTION',
+            'DEFECT_LEVEL',
+            'OQC_DETECTION',
+            'WARNINGS',
+            'OTHERS',
+        ],
+        {
+            errorMap: () => ({
+                message:
+                    'Type must be CLEANING_5S, DISCIPLINE, COMMITMENT, IMPROVEMENTS, AUDITS, DETECTION, DEFECT_LEVEL, OQC_DETECTION, WARNINGS or OTHERS.',
+            }),
+        },
+    ),
+    score: z
+        .number({
+            required_error: 'Score is required.',
+            invalid_type_error: 'Score must be a number.',
+        })
+        .min(1, {
+            message: 'Score must be greater than 0.',
+        })
+        .refine((value) =>
+            validator.isCurrency(value.toFixed(2), {
+                digits_after_decimal: [2],
+                allow_negatives: false,
+                decimal_separator: '.',
+            }),
+        ),
+    date: z
+        .string({
+            required_error: 'Date is required.',
+        })
+        .datetime({
+            message: 'Date must be a valid date.',
+        }),
+})
+
+export const updateAvaliationSchema = createAvaliationSchema
+    .omit({
+        user_id: true,
+    })
+    .partial()
+    .strict({
+        message: 'Some provided field is not allowed.',
+    })
