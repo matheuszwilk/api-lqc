29809bd0a9d899990b2c3a50f079e095a99c90e2
matheuszwilk <matheuszwilk@gmail.com>
2024-03-26 10:35:32 -0300

    feat: ensure UpdateUserLqcController

---
+++ src/controllers/user-lqc/update-user-lqc.js
@@ -0,0 +1,60 @@
+import {
+    RegistrationAlreadyInUseError,
+    UserNotFoundError,
+} from '../../errors/user.js'
+import { updateUserSchema } from '../../schemas/user.js'
+import {
+    checkIfIdIsValid,
+    invalidIdResponse,
+    badRequest,
+    ok,
+    serverError,
+    userNotFoundResponse,
+} from '../helpers/index.js'
+import { ZodError } from 'zod'
+
+export class UpdateUserLqcController {
+    constructor(updateUserLqcUseCase) {
+        this.updateUserLqcUseCase = updateUserLqcUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const userLqcId = httpRequest.params.userId
+
+            const isIdValid = checkIfIdIsValid(userLqcId)
+
+            if (!isIdValid) {
+                return invalidIdResponse()
+            }
+
+            const params = httpRequest.body
+
+            await updateUserSchema.parseAsync(params)
+
+            const updatedUserLqc = await this.updateUserLqcUseCase.execute(
+                userLqcId,
+                params,
+            )
+
+            return ok(updatedUserLqc)
+        } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                })
+            }
+
+            if (error instanceof RegistrationAlreadyInUseError) {
+                return badRequest({ message: error.message })
+            }
+
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse()
+            }
+
+            console.error(error)
+            return serverError()
+        }
+    }
+}
