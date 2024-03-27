dd68b50734669b78c981f10df7495f4f1a60369f
matheuszwilk <matheuszwilk@gmail.com>
2024-03-27 17:40:03 -0300

    feat: add update-avaliation in index.js import

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,18 +1,20 @@
 export * from './user/create-user.js'
 export * from './user/get-user-by-id.js'
-export * from './user/update-user.js'
 export * from './user/delete-user.js'
+export * from './user/update-user.js'
 
 export * from './transaction/create-transaction.js'
 export * from './transaction/get-transactions-by-user-id.js'
-export * from './transaction/update-transaction.js'
 export * from './transaction/delete-transaction.js'
+export * from './transaction/update-transaction.js'
 
 export * from './user-lqc/create-user-lqc.js'
 export * from './user-lqc/get-user-lqc.js'
 export * from './user-lqc/get-user-lqc-by-id.js'
+export * from './user-lqc/delete-user-lqc.js'
 export * from './user-lqc/update-user-lqc.js'
 
 export * from './avaliation/create-avaliation.js'
 export * from './avaliation/get-avaliation-by-user-id.js'
 export * from './avaliation/delete-avaliation.js'
+export * from './avaliation/update-avaliation.js'
