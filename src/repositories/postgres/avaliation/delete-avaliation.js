5d964b70625a74faa0866a887c4320e6ca142cdd
matheuszwilk <matheuszwilk@gmail.com>
2024-03-27 15:46:00 -0300

    feat: ensure PostgresDeleteAvaliationRepository

---
+++ src/repositories/postgres/avaliation/delete-avaliation.js
@@ -0,0 +1,24 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library.js'
+import { prisma } from '../../../../prisma/prisma.js'
+import { TransactionNotFoundError } from '../../../errors/index.js'
+
+export class PostgresDeleteAvaliationRepository {
+    async execute(userId) {
+        try {
+            return await prisma.avaliation.delete({
+                where: {
+                    id: userId,
+                },
+            })
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                // P2025 = "An operation failed because it depends on one or more records that were required but not found" (from Prisma docs)
+                if (error.code === 'P2025') {
+                    throw new TransactionNotFoundError(userId)
+                }
+            }
+
+            throw error
+        }
+    }
+}
