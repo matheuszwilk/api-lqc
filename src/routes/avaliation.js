053d1fdb8e036eef537d076bbf1900096a1daea9
matheuszwilk <matheuszwilk@gmail.com>
2024-03-27 10:28:49 -0300

    feat: add makeGetAvaliationController at routes

--- src/routes/avaliation.js
+++ src/routes/avaliation.js
@@ -1,10 +1,22 @@
 import { Router } from 'express'
-import { makeCreateAvaliationController } from '../factories/controllers/avaliation.js'
+import {
+    makeCreateAvaliationController,
+    makeGetAvaliationController,
+} from '../factories/controllers/avaliation.js'
 
 export const avaliationRouter = Router()
 
 avaliationRouter.post('/', async (request, response) => {
     const createAvaliationController = makeCreateAvaliationController()
+
+    const { statusCode, body } =
+        await createAvaliationController.execute(request)
+
+    response.status(statusCode).send(body)
+})
+
+avaliationRouter.get('/:userLqcId', async (request, response) => {
+    const createAvaliationController = makeGetAvaliationController()
 
     const { statusCode, body } =
         await createAvaliationController.execute(request)
