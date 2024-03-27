8d937ee94eb37a55a19d2c657315f7723bc5aefd
matheuszwilk <matheuszwilk@gmail.com>
2024-03-27 17:36:25 -0300

    feat: ensure UpdateAvaliationController

---
+++ src/controllers/avaliation/update-avaliation.js
@@ -0,0 +1,51 @@
+import { ZodError } from 'zod'
+import { updateAvaliationSchema } from '../../schemas/index.js'
+import {
+    checkIfIdIsValid,
+    invalidIdResponse,
+    serverError,
+    ok,
+    badRequest,
+    AvaliationNotFoundResponse,
+} from '../helpers/index.js'
+import { AvaliationNotFoundError } from '../../errors/avaliation.js'
+
+export class UpdateAvaliationController {
+    constructor(updateAvaliationUseCase) {
+        this.updateAvaliationUseCase = updateAvaliationUseCase
+    }
+    async execute(httpRequest) {
+        try {
+            const idIsValid = checkIfIdIsValid(httpRequest.params.userId)
+
+            if (!idIsValid) {
+                return invalidIdResponse()
+            }
+
+            const updateAvaliationParams = httpRequest.body
+
+            await updateAvaliationSchema.parseAsync(updateAvaliationParams)
+
+            const transaction = await this.updateAvaliationUseCase.execute(
+                httpRequest.params.userId,
+                updateAvaliationParams,
+            )
+
+            return ok(transaction)
+        } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                })
+            }
+
+            if (error instanceof AvaliationNotFoundError) {
+                return AvaliationNotFoundResponse()
+            }
+
+            console.error(error)
+
+            return serverError()
+        }
+    }
+}
