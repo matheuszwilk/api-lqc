d27bdbce81e5063f1a8ca786e6d6e4ebdab6e7ab
Matheus Pereira <matheuszwilk@gmail.com>
2024-03-29 15:54:59 -0400

    feat: ensure UpdateWorkStationController

---
+++ src/controllers/workstation/update-workstation.js
@@ -0,0 +1,50 @@
+import { ZodError } from 'zod'
+import { updateWorkstationSchema } from '../../schemas/index.js'
+import {
+    checkIfIdIsValid,
+    invalidIdResponse,
+    serverError,
+    ok,
+    badRequest,
+    WorkstationNotFoundResponse,
+} from '../helpers/index.js'
+import { WorkstationNotFoundError } from '../../errors/workstation.js'
+
+export class UpdateWorkStationController {
+    constructor(updateWorkStationUseCase) {
+        this.updateWorkStationUseCase = updateWorkStationUseCase
+    }
+    async execute(httpRequest) {
+        try {
+            const idIsValid = checkIfIdIsValid(httpRequest.params.userId)
+
+            if (!idIsValid) {
+                return invalidIdResponse()
+            }
+
+            const updateWorkStationParams = httpRequest.body
+
+            await updateWorkstationSchema.parseAsync(updateWorkStationParams)
+
+            const workstation = await this.updateAvaliationUseCase.execute(
+                httpRequest.params.userId,
+                updateWorkStationParams,
+            )
+
+            return ok(workstation)
+        } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                })
+            }
+
+            if (error instanceof WorkstationNotFoundError) {
+                return WorkstationNotFoundResponse()
+            }
+
+            console.error(error)
+            return serverError()
+        }
+    }
+}
