58834edb9507da33f095daa62e12b95263f7c901
Matheus Pereira <matheuszwilk@gmail.com>
2024-03-29 15:47:28 -0400

    feat: ensure PostgresUpdateWorkStationRepository

---
+++ src/repositories/postgres/workstation/update-workstation.js
@@ -0,0 +1,25 @@
+import { prisma } from '../../../../prisma/prisma.js'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library.js'
+import { WorkstationNotFoundError } from '../../../errors/workstation.js'
+
+export class PostgresUpdateWorkStationRepository {
+    async execute(userId, updateWorkStationParams) {
+        try {
+            return await prisma.workStation.update({
+                where: {
+                    id: userId,
+                },
+                data: updateWorkStationParams,
+            })
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                // P2025 = "An operation failed because it depends on one or more records that were required but not found" (from Prisma docs)
+                if (error.code === 'P2025') {
+                    throw new WorkstationNotFoundError(userId)
+                }
+            }
+
+            throw error
+        }
+    }
+}
