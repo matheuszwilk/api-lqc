9e9ce1fe886d3fb2043477aaa6953dbfa0d1cba2
Matheus Pereira <matheuszwilk@gmail.com>
2024-03-29 09:46:01 -0400

    feat: add makeDeleteWorkstationController at routes

--- src/routes/workstation.js
+++ src/routes/workstation.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import {
     makeCreateWorkstationController,
+    makeDeleteWorkstationController,
     makeGetWorkstationController,
 } from '../factories/controllers/workstation.js'
 
@@ -19,6 +20,15 @@ workstationRouter.get('', async (request, response) => {
     const getWorkstationController = makeGetWorkstationController()
 
     const { statusCode, body } = await getWorkstationController.execute(request)
+
+    response.status(statusCode).send(body)
+})
+
+workstationRouter.delete('/:workstationId', async (request, response) => {
+    const deleteWorkstationController = makeDeleteWorkstationController()
+
+    const { statusCode, body } =
+        await deleteWorkstationController.execute(request)
 
     response.status(statusCode).send(body)
 })
