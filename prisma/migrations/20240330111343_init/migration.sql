bc7478d8f391b76384ff6134ad7ac78b02013948
Matheus Pereira <matheuszwilk@gmail.com>
2024-03-30 07:18:09 -0400

    feat: add imageUrl

--- prisma/migrations/20240330111343_init/migration.sql
+++ prisma/migrations/20240330111343_init/migration.sql
@@ -30,6 +30,7 @@ CREATE TABLE "User_LQC" (
     "registration" VARCHAR(50) NOT NULL,
     "position" "position_type" NOT NULL,
     "admission_date" DATE NOT NULL,
+    "imageUrl" TEXT NOT NULL,
 
     CONSTRAINT "User_LQC_pkey" PRIMARY KEY ("id")
 );
@@ -37,11 +38,11 @@ CREATE TABLE "User_LQC" (
 -- CreateTable
 CREATE TABLE "Avaliation" (
     "id" UUID NOT NULL,
-    "user_id" UUID NOT NULL,
     "user_lqc_id" UUID NOT NULL,
     "type" "avaliation_type" NOT NULL,
     "score" DECIMAL(2,0) NOT NULL,
     "date" DATE NOT NULL,
+    "imageUrl" TEXT,
     "comment" VARCHAR(2000) NOT NULL,
 
     CONSTRAINT "Avaliation_pkey" PRIMARY KEY ("id")
@@ -50,11 +51,10 @@ CREATE TABLE "Avaliation" (
 -- CreateTable
 CREATE TABLE "WorkStation" (
     "id" UUID NOT NULL,
-    "user_id" UUID NOT NULL,
     "user_lqc_id" UUID NOT NULL,
     "station_type" "station_name_type" NOT NULL,
     "line_type" "line_type" NOT NULL,
-    "Able" BOOLEAN NOT NULL,
+    "Able" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "WorkStation_pkey" PRIMARY KEY ("id")
 );
@@ -69,13 +69,7 @@ CREATE UNIQUE INDEX "User_LQC_registration_key" ON "User_LQC"("registration");
 ALTER TABLE "User_LQC" ADD CONSTRAINT "User_LQC_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "Avaliation" ADD CONSTRAINT "Avaliation_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
-
--- AddForeignKey
 ALTER TABLE "Avaliation" ADD CONSTRAINT "Avaliation_user_lqc_id_fkey" FOREIGN KEY ("user_lqc_id") REFERENCES "User_LQC"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
-
--- AddForeignKey
-ALTER TABLE "WorkStation" ADD CONSTRAINT "WorkStation_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- AddForeignKey
 ALTER TABLE "WorkStation" ADD CONSTRAINT "WorkStation_user_lqc_id_fkey" FOREIGN KEY ("user_lqc_id") REFERENCES "User_LQC"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
---
+++ prisma/migrations/20240330111629_refactor/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Avaliation" ALTER COLUMN "comment" DROP NOT NULL;
