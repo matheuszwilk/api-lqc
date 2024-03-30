78254eedee53b657ac9b3381acc6822c9b2021fc
Matheus Pereira <matheuszwilk@gmail.com>
2024-03-30 07:54:26 -0400

    refactor: removing excess code

--- src/repositories/postgres/user-lqc/create-user-lqc.js
+++ src/repositories/postgres/user-lqc/create-user-lqc.js
@@ -3,17 +3,8 @@ import { prisma } from '../../../../prisma/prisma.js'
 export class PostgresCreateUserLqcRepository {
     async execute(createUserParams) {
         const userLQC = await prisma.user_LQC.create({
-            data: {
-                id: createUserParams.id,
-                user_id: createUserParams.user_id,
-                first_name: createUserParams.first_name,
-                last_name: createUserParams.last_name,
-                registration: createUserParams.registration,
-                position: createUserParams.position,
-                admission_date: createUserParams.admission_date,
-            },
+            data: createUserParams,
         })
-
         return userLQC
     }
 }
--- src/repositories/postgres/user/create-user.js
+++ src/repositories/postgres/user/create-user.js
@@ -3,15 +3,8 @@ import { prisma } from '../../../../prisma/prisma.js'
 export class PostgresCreateUserRepository {
     async execute(createUserParams) {
         const user = await prisma.user.create({
-            data: {
-                id: createUserParams.id,
-                email: createUserParams.email,
-                first_name: createUserParams.first_name,
-                last_name: createUserParams.last_name,
-                password: createUserParams.password,
-            },
+            data: createUserParams,
         })
-
         return user
     }
 }
